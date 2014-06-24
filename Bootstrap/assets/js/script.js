3b828fa2ebd23fe011c2649ba2b2426ba98c5527
shyamsathyanathan <shyamsathyanathan@yahoo.in>
2014-06-24 17:01:16 +0530

    More problem

--- Bootstrap/assets/js/script.js
+++ Bootstrap/assets/js/script.js
@@ -70,12 +70,4 @@ $(document).ready(function() {
     });
 
 });
-
-$(document).on("scroll",function(){
-    if($(document).scrollTop()>100){
-        $("header").removeClass("large").addClass("small");
-    } else{
-        $("header").removeClass("small").addClass("large");
-    }
-});
 </script>
